61a791aaabb9328d0c9ba34a896ed319a45155e7
Fernandez de Labastida Mikel <MFernandezLabastida@ikerlan.es>
2024-04-19 23:39:40 +0200

    modify proyect styles

--- src/components/AnimatedButton.jsx
+++ src/components/AnimatedButton.jsx
@@ -1,62 +1,109 @@
 'use client';
 import { useRef, useState } from 'react';
+// import GitHub from '../icons/GitHub.astro';
+// import Link from '../icons/Link.astro';
 
-const AnimatedButton = ({children, href}) => {
-  const divRef = useRef(null);
-  const [isFocused, setIsFocused] = useState(false);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [opacity, setOpacity] = useState(0);
+const AnimatedButton = ({ children, href, func }) => {
+    const divRef = useRef(null);
+    const [isFocused, setIsFocused] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [opacity, setOpacity] = useState(0);
 
-  const handleMouseMove = (e) => {
-    if (!divRef.current || isFocused) return;
+    const handleMouseMove = (e) => {
+        if (!divRef.current || isFocused) return;
 
-    const div = divRef.current;
-    const rect = div.getBoundingClientRect();
+        const div = divRef.current;
+        const rect = div.getBoundingClientRect();
 
-    setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
-  };
+        setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+    };
 
-  const handleFocus = () => {
-    setIsFocused(true);
-    setOpacity(1);
-  };
+    const handleFocus = () => {
+        setIsFocused(true);
+        setOpacity(1);
+    };
 
-  const handleBlur = () => {
-    setIsFocused(false);
-    setOpacity(0);
-  };
+    const handleBlur = () => {
+        setIsFocused(false);
+        setOpacity(0);
+    };
 
-  const handleMouseEnter = () => {
-    setOpacity(1);
-  };
+    const handleMouseEnter = () => {
+        setOpacity(1);
+    };
 
-  const handleMouseLeave = () => {
-    setOpacity(0);
-  };
+    const handleMouseLeave = () => {
+        setOpacity(0);
+    };
 
-  return (
-    <>
-      <a
-        href={href}
-        ref={divRef}
-        onMouseMove={handleMouseMove}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        className='relative inline-flex h-10 items-center justify-center overflow-hidden rounded-md border border-gray-800 bg-gradient-to-r from-gray-900 to-gray-950 px-6 font-medium text-gray-300 shadow-2xl transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50'
-      >
-        <div
-          className='pointer-events-none absolute -inset-px opacity-0 transition duration-500'
-          style={{
-            opacity,
-            background: `radial-gradient(300px circle at ${position.x}px ${position.y}px, rgba(255,255,255,.1), #0000000f)`,
-          }}
-        />
-        {children}
-      </a>
-    </>
-  );
+    return (
+        <>
+            <a
+                href={href}
+                ref={divRef}
+                onMouseMove={handleMouseMove}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                className='relative inline-flex gap-2 h-10 items-center justify-center overflow-hidden rounded-md border border-gray-800 bg-gradient-to-r from-gray-900 to-gray-950 px-5 font-medium text-gray-300 shadow-2xl transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50'
+            >
+                <div
+                    className='pointer-events-none absolute -inset-px opacity-0 transition duration-500'
+                    style={{
+                        opacity,
+                        background: `radial-gradient(300px circle at ${position.x}px ${position.y}px, rgba(255,255,255,.1), #0000000f)`,
+                    }}
+                />
+                {func == 'code' ? (
+                    <GitHub/>
+                ) : (
+                    <Link/>
+                )}
+                {children}
+            </a>
+        </>
+    );
 };
 
+function GitHub() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-brand-github size-4"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
+            d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"
+        ></path></svg>
+    )
+}
+
+function Link() {
+    return(
+        <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    class="link size-4"
+    ><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"
+    ></path><path
+        d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"
+    ></path></svg>
+    )
+}
+
 export default AnimatedButton;
+
